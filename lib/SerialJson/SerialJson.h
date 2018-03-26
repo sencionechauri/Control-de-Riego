d0f4a1dcb5d5b77a36b4e6188d5d63e765fbf9b2
micheldavalos <michel.davalos@academicos.udg.mx>
2018-03-26 12:23:48 -0600

    creada la clase SerialJson

---
+++ lib/SerialJson/SerialJson.h
@@ -0,0 +1,39 @@
+//
+// Created by Michel on 26/03/18.
+//
+
+#ifndef CONTROL_DE_RIEGO_SERIALJSON_H
+#define CONTROL_DE_RIEGO_SERIALJSON_H
+
+#include <ArduinoJson.h>
+
+class SerialJson {
+    StaticJsonBuffer<512> jsonBuffer;
+public:
+    String inputString = "";         // a string to hold incoming data
+    boolean stringComplete = false;  // whether the string is complete
+    SerialJson() {}
+
+    void check() {
+        if (stringComplete) {
+            // Serial.println(inputString);
+            // Serial.println(inputString.length());
+            JsonObject& root = jsonBuffer.parse(inputString);
+            // root.printTo(Serial);
+
+            uint8_t tipo = root["tipo"];
+            uint32_t valor = root["valor"];
+            Serial.println(tipo);
+            Serial.println(valor);
+            jsonBuffer.clear();
+//    core(tipo, valor);
+            // clear the string:
+            inputString = "";
+            stringComplete = false;
+        }
+    }
+
+};
+
+
+#endif //CONTROL_DE_RIEGO_SERIALJSON_H
--- src/sensor_humedad_clases.ino
+++ src/sensor_humedad_clases.ino
@@ -1,5 +1,6 @@
 #include "SensorHumedadManager.h"
 #include "../../../.platformio/packages/toolchain-atmelavr/avr/include/avr/io.h"
+#include "../../../.platformio/packages/framework-arduinoavr/cores/arduino/HardwareSerial.h"
 #include <SimpleTimer.h>
 
 SimpleTimer timerEspera;
@@ -19,56 +20,53 @@ static const int BOMBA_ON = 1;
  * Para usar JSON
  */
 #include <SoftwareSerial.h>
-#include <ArduinoJson.h>
-StaticJsonBuffer<512> jsonBuffer;
-String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
-void jsonVerify();
+#include <SerialJson.h>
+SerialJson serialJson;
 void core();
 
 
 void setup()
 {
-  pinMode(bombaPin, INPUT);
-  Serial.begin(115200);
-  Serial.println("Reading from the sensor...");
-  timerEsperaId = timerEspera.setInterval(HORA*6, idle);
-  delay(20000);
-  sense();
+    pinMode(bombaPin, INPUT);
+    Serial.begin(115200);
+    Serial.println("Reading from the sensor...");
+    timerEsperaId = timerEspera.setInterval(HORA*6, idle);
+//  delay(20000);
+//  sense();
 }
 
 void sense()
 {
-  long time = 0;
-  int av;
-  average = 0;
-  muestras = 0;
-  while (muestras < 10)
-  {
-    if (millis() % SEGUNDO == 0)
+    long time = 0;
+    int av;
+    average = 0;
+    muestras = 0;
+    while (muestras < 10)
     {
-      //Serial.println("Sensando");
-      av = manager.doSensing();
-      average += av;
-      muestras++;
+        if (millis() % SEGUNDO == 0)
+        {
+            //Serial.println("Sensando");
+            av = manager.doSensing();
+            average += av;
+            muestras++;
+        }
     }
-  }
-  Serial.print("Final: ");
-  Serial.println(average / 10);
-  Serial.print("Bomba: ");
-  Serial.println(digitalRead(bombaPin));
+    Serial.print("Final: ");
+    Serial.println(average / 10);
+    Serial.print("Bomba: ");
+    Serial.println(digitalRead(bombaPin));
 }
 
 void idle()
 {
-  Serial.println("Iniciando");
-  sense();
+    Serial.println("Iniciando");
+    sense();
 }
 
 void loop()
 {
-  timerEspera.run();
-  jsonVerify();
+    timerEspera.run();
+    serialJson.check();
 }
 
 /*!
@@ -76,56 +74,36 @@ void loop()
  */
 
 void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag
-    // so the main loop can do something about it:
-    if (inChar == '}') {
-      stringComplete = true;
+    while (Serial.available()) {
+        // get the new byte:
+        char inChar = (char)Serial.read();
+        // add it to the inputString:
+        serialJson.inputString += inChar;
+        // if the incoming character is a newline, set a flag
+        // so the main loop can do something about it:
+        if (inChar == '}') {
+            serialJson.stringComplete = true;
+        }
     }
-  }
-}
-
-
-void jsonVerify() {
-  if (stringComplete) {
-    // Serial.println(inputString);
-    // Serial.println(inputString.length());
-    JsonObject& root = jsonBuffer.parse(inputString);
-    // root.printTo(Serial);
-
-    uint8_t tipo = root["tipo"];
-    uint32_t valor = root["valor"];
-    Serial.println(tipo);
-    Serial.println(valor);
-    jsonBuffer.clear();
-//    core(tipo, valor);
-    // clear the string:
-    inputString = "";
-    stringComplete = false;
-  }
 }
 
 void core(uint8_t tipo, uint32_t valor)
 {
-  if (tipo == 1) {  /// relevador
+    if (tipo == 1) {  /// relevador
 //    if (valor == 0) {
 //      relay.off();
 //    } else {
 //      relay.on();
 //    }
-  }
-  else if (tipo == 2) { /// bomba
+    }
+    else if (tipo == 2) { /// bomba
 //        bomba.velocity(valor);
-  }
-  else if (tipo == 3) {
+    }
+    else if (tipo == 3) {
 //    if (valor == 0) {
 //      aSwitch.off();
 //    } else {
 //      aSwitch.on();
 //    }
-  }
+    }
 }
